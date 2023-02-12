5f5993d012fce71d75428c4edccac1175aef0857
Leonardo Garcia <leogarcia258@gmail.com>
2023-02-12 13:14:52 -0800

    adding a 404 page

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.render('Home')
 })
 app.get('*', (req, res) => {
-    res.status(404) .send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 app.listen(process.env.PORT)
